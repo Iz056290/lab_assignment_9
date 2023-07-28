a1b8a25acc6b5d33b8e0c2bb60bad4d9924fa495
Iz056290 <Izayhacw@knights.ucf.edu>
2023-07-28 13:18:29 -0400

    Implemented Hash
    The hash function now takes two parameters. The main function initializes the hashSize variable with a value (in this case, 10). The code creates the hash table by dynamically allocating memory for the struct HashType. It then reads the records from "input.txt" using the parseData function and stores them in an array of struct RecordType. The insertRecord function is used to insert each record into the hash table using separate chaining. after inserting all records into the hash table, the displayRecordsInHash function is called to print the contents. Lastly, the code frees the memory.

--- lab9.c
+++ lab9.c
@@ -1,24 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // RecordType
 struct RecordType
 {
-	int		id;
-	char	name;
-	int		order; 
+    int     id;
+    char    name;
+    int     order;
+};
+
+struct Node
+{
+    struct RecordType record;
+    struct Node* next;
 };
 
 // Fill out this structure
 struct HashType
 {
-
+    struct Node* head;
 };
 
 // Compute the hash function
-int hash(int x)
+int hash(int x, int hashSize)
 {
-
+    return x % hashSize;
 }
+
+void insertRecord(struct HashType* hashTable, int hashSize, struct RecordType record)
+{
+    int index = hash(record.id, hashSize);
+
+    // Create a new node for the record
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->record = record;
+    newNode->next = NULL;
+
+    // If the slot in the hash table is empty, simply add the new node
+    if (hashTable[index].head == NULL)
+    {
+        hashTable[index].head = newNode;
+    }
+    else // If the slot is not empty, add the new node to the front of the linked list
+    {
+        newNode->next = hashTable[index].head;
+        hashTable[index].head = newNode;
+    }
+}
+
 
 // parses input file to an integer array
 int parseData(char* inputFileName, struct RecordType** ppData)
@@ -73,22 +102,78 @@ void printRecords(struct RecordType pData[], int dataSz)
 // skip the indices which are free
 // the output will be in the format:
 // index x -> id, name, order -> id, name, order ....
-void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
+void displayRecordsInHash(struct HashType* pHashArray, int hashSz)
 {
-	int i;
-
-	for (i=0;i<hashSz;++i)
-	{
-		// if index is occupied with any records, print all
-	}
+    for (int i = 0; i < hashSz; ++i)
+    {
+        printf("index %d ->", i);
+        struct Node* current = pHashArray[i].head;
+        while (current != NULL)
+        {
+            printf(" %d %c %d ->", current->record.id, current->record.name, current->record.order);
+            current = current->next;
+        }
+        printf(" NULL\n");
+    }
 }
+
+void freeHashTable(struct HashType* pHashArray, int hashSz)
+{
+    for (int i = 0; i < hashSz; ++i)
+    {
+        struct Node* current = pHashArray[i].head;
+        while (current != NULL)
+        {
+            struct Node* temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    free(pHashArray);
+}
+
 
 int main(void)
 {
-	struct RecordType *pRecords;
-	int recordSz = 0;
+    struct RecordType* pRecords;
+    int recordSz = 0;
 
-	recordSz = parseData("input.txt", &pRecords);
-	printRecords(pRecords, recordSz);
-	// Your hash implementation
+    recordSz = parseData("input.txt", &pRecords);
+    printRecords(pRecords, recordSz);
+
+    int hashSize = 10; // You can adjust this size according to your requirements
+
+    // Create the hash table
+    struct HashType* hashTable = (struct HashType*)malloc(sizeof(struct HashType) * hashSize);
+    for (int i = 0; i < hashSize; ++i)
+    {
+        hashTable[i].head = NULL;
+    }
+
+    // Your hash implementation
+    for (int i = 0; i < recordSz; ++i)
+    {
+        insertRecord(hashTable, hashSize, pRecords[i]);
+    }
+
+    // Display records in the hash table
+    displayRecordsInHash(hashTable, hashSize);
+
+    // Free memory used by the hash table and individual linked lists
+    for (int i = 0; i < hashSize; ++i)
+    {
+        struct Node* current = hashTable[i].head;
+        while (current != NULL)
+        {
+            struct Node* temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    free(hashTable);
+
+    // Free memory used by the records array
+    free(pRecords);
+
+    return 0;
 }
